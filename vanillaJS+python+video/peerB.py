49790e0234a5b314a6783f082016874a8f2db13d
benamager <benaprivat@gmail.com>
2024-01-20 01:21:07 +0100

    Add graceful shutdown

--- vanillaJS+python+video/peerB.py
+++ vanillaJS+python+video/peerB.py
@@ -17,11 +17,6 @@ CAMERA_SIZE = (640, 480)
 SIGNALING_SERVER_URL = 'http://192.168.1.139:8080'
 SIGNALING_SERVER_TOKEN = "secureTokenGoesHere"
 
-# Camera instance
-picam2 = Picamera2()
-picam2.configure(picam2.create_video_configuration(main={"size": CAMERA_SIZE}))
-picam2.start()
-
 class Picamera2Track(VideoStreamTrack):
     def __init__(self, camera):
         super().__init__()
@@ -58,105 +53,127 @@ def create_peer_connection():
     return RTCPeerConnection(configuration=RTCConfiguration([RTCIceServer("stun:fr-turn1.xirsys.com")]))
 
 async def main():
-    peer_connection = create_peer_connection()
+    sio = None
+    peer_connection = None
+    picam2 = None
 
-    # Socket.IO client
-    sio = socketio.AsyncClient()
-
-    @sio.event
-    async def connect():
-        print("Connected to the signaling server")
-
-    @sio.event
-    async def disconnect():
-        print("Disconnected from the server")
-        await peer_connection.close()
-
-    @sio.on('offer')
-    async def handle_offer(offer_json):
-        print('Received offer from Peer A')
-        offer = json.loads(offer_json)
-
-        # Send candidate to Peer A over signaling channel
-        @peer_connection.on("ice_candidate")
-        async def on_icecandidate(candidate):
-            await sio.emit('ice_candidate', json.dumps({
-                "candidate": candidate.candidate,
-                "sdpMid": candidate.sdpMid,
-                "sdpMLineIndex": candidate.sdpMLineIndex
-            }))
-
-        # Handle Data Channel
-        @peer_connection.on("datachannel")
-        def on_data_channel(channel):
-            @channel.on("message")
-            def on_message(message):
-                print("Received message:", message)
-
-            @channel.on("open")
-            def on_open():
-                print("dataChannel opened")
-
-            @channel.on("close")
-            def on_close():
-                print("dataChannel closed")
-
-        # Set remote description from Peer A
-        try:
-            await peer_connection.setRemoteDescription(RTCSessionDescription(sdp=offer["sdp"], type=offer["type"]))
-        except Exception as e:
-            print(f"Error setting remote description: {e}")
-
-        video_track = Picamera2Track(picam2)
-        peer_connection.addTrack(video_track)
-
-        # Create and set local answer
-        try:
-            local_answer = await peer_connection.createAnswer()
-            await peer_connection.setLocalDescription(local_answer)
-        except Exception as e:
-            print(f"Error creating or setting local answer: {e}")
-
-        await sio.emit('answer', json.dumps({"sdp": peer_connection.localDescription.sdp, "type": peer_connection.localDescription.type}))
-
-    # Handle ICE candidate messages
-    @sio.on('ice_candidate')
-    async def handle_icecandidate(data):
-        try:
-            print('Received ICE candidate from Peer A:', data)
-            candidate_data = json.loads(data)
-
-            # Parse the ICE candidate string
-            parts = candidate_data['candidate'].split()
-
-            # Create an RTCIceCandidate object
-            ice_candidate = RTCIceCandidate(
-                foundation=parts[0],
-                component=int(parts[1]),
-                protocol=parts[2].lower(),
-                priority=int(parts[3]),
-                ip=parts[4],
-                port=int(parts[5]),
-                type=parts[7],
-                sdpMid=candidate_data.get('sdpMid'),
-                sdpMLineIndex=candidate_data.get('sdpMLineIndex')
-            )
-
-            await peer_connection.addIceCandidate(ice_candidate)
-            print("Added ICE candidate")
-        except Exception as e:
-            print(f"Error handling ICE candidate: {e}")
-
-    # Connect to signaling server
     try:
-        await sio.connect(SIGNALING_SERVER_URL, 
-                    query={"token": SIGNALING_SERVER_TOKEN})
-    except Exception as e:
-        print(f"Error connecting to signaling server: {e}")
-        return
+        # Camera instance
+        picam2 = Picamera2()
+        picam2.configure(picam2.create_video_configuration(main={"size": CAMERA_SIZE}))
+        picam2.start()
 
-    # Keep application running
-    await sio.wait()
+        # Wait for camera to warm up
+        await asyncio.sleep(2)
+        peer_connection = create_peer_connection()
+
+        # Socket.IO client
+        sio = socketio.AsyncClient()
+
+        @sio.event
+        async def connect():
+            print("Connected to the signaling server")
+
+        @sio.event
+        async def disconnect():
+            print("Disconnected from the server")
+            await peer_connection.close()
+
+        @sio.on('offer')
+        async def handle_offer(offer_json):
+            print('Received offer from Peer A')
+            offer = json.loads(offer_json)
+
+            # Send candidate to Peer A over signaling channel
+            @peer_connection.on("ice_candidate")
+            async def on_icecandidate(candidate):
+                await sio.emit('ice_candidate', json.dumps({
+                    "candidate": candidate.candidate,
+                    "sdpMid": candidate.sdpMid,
+                    "sdpMLineIndex": candidate.sdpMLineIndex
+                }))
+
+            # Handle Data Channel
+            @peer_connection.on("datachannel")
+            def on_data_channel(channel):
+                @channel.on("message")
+                def on_message(message):
+                    print("Received message:", message)
+
+                @channel.on("open")
+                def on_open():
+                    print("dataChannel opened")
+
+                @channel.on("close")
+                def on_close():
+                    print("dataChannel closed")
+
+            # Set remote description from Peer A
+            try:
+                await peer_connection.setRemoteDescription(RTCSessionDescription(sdp=offer["sdp"], type=offer["type"]))
+            except Exception as e:
+                print(f"Error setting remote description: {e}")
+
+            video_track = Picamera2Track(picam2)
+            peer_connection.addTrack(video_track)
+
+            # Create and set local answer
+            try:
+                local_answer = await peer_connection.createAnswer()
+                await peer_connection.setLocalDescription(local_answer)
+            except Exception as e:
+                print(f"Error creating or setting local answer: {e}")
+
+            await sio.emit('answer', json.dumps({"sdp": peer_connection.localDescription.sdp, "type": peer_connection.localDescription.type}))
+
+        # Handle ICE candidate messages
+        @sio.on('ice_candidate')
+        async def handle_icecandidate(data):
+            try:
+                print('Received ICE candidate from Peer A:', data)
+                candidate_data = json.loads(data)
+
+                # Parse the ICE candidate string
+                parts = candidate_data['candidate'].split()
+
+                # Create an RTCIceCandidate object
+                ice_candidate = RTCIceCandidate(
+                    foundation=parts[0],
+                    component=int(parts[1]),
+                    protocol=parts[2].lower(),
+                    priority=int(parts[3]),
+                    ip=parts[4],
+                    port=int(parts[5]),
+                    type=parts[7],
+                    sdpMid=candidate_data.get('sdpMid'),
+                    sdpMLineIndex=candidate_data.get('sdpMLineIndex')
+                )
+
+                await peer_connection.addIceCandidate(ice_candidate)
+                print("Added ICE candidate")
+            except Exception as e:
+                print(f"Error handling ICE candidate: {e}")
+
+        # Connect to signaling server
+        try:
+            await sio.connect(f"{SIGNALING_SERVER_URL}/?token={SIGNALING_SERVER_TOKEN}")
+        except Exception as e:
+            print(f"Error connecting to signaling server: {e}")
+            return
+
+        # Keep application running
+        await sio.wait()
+    finally:
+        if sio is not None:
+            await sio.disconnect()
+            print("Disconnected from signaling server")
+        if peer_connection is not None:
+            await peer_connection.close()
+            print("Closed peer connection")
+        if picam2 is not None:
+            picam2.stop()
+            print("Stopped camera")
+        print("Graceful shutdown complete")
 
 if __name__ == '__main__':
     asyncio.run(main())
